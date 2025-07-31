0b877e7619a52d9669b87453f0370d0626865bf2
Ashok Shah <bayes.ashok@gmail.com>
2025-07-31 15:00:04 +0545

    added securedbuild

--- web-api-sikshyalaya-client/vite.config.js
+++ web-api-sikshyalaya-client/vite.config.js
@@ -2,9 +2,28 @@ import path from "path";
 import fs from "fs";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import obfuscator from "rollup-plugin-obfuscator";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  build: {
+    outDir: 'dist',
+    minify: 'esbuild',
+    rollupOptions: {
+      plugins: [
+        obfuscator({
+          compact: true,
+          controlFlowFlattening: true,
+          deadCodeInjection: true,
+          stringArray: true,
+          rotateStringArray: true,
+          disableConsoleOutput: true
+        }, ['**/*.css']) // don't obfuscate CSS
+      ]
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
